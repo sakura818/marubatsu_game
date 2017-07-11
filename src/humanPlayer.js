c5cbb080ba1c8eef57ff2f32e4d4921217581473
sakura818 <haruka.sugawara@topgate.co.jp>
2017-07-11 18:07:48 +0900

    humanPlayer.js

---
+++ src/humanPlayer.js
@@ -0,0 +1,20 @@
+/**
+ * 人間のプレイヤー
+ *
+ */
+export default class HumanPlayer {
+    constructor(playerId) {
+        this.playerId = playerId;
+    }
+
+    /**
+     * ユーザーが選択した場合に呼び出される関数
+     * もしすでにまるかばつが置かれていたらアラートをだす
+     */
+    select(board, Ui, x, y) {
+        if (board.isAlreadyPut(x, y)) {
+            Ui.alertAlreadyPutMessage();
+        }
+        board.put(x, y, this.playerId);
+    }
+}
